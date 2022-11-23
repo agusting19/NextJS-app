f336de2f33f209164a31a456cd276b4e7c12a2f8
Agustin Gomez <agusleongomez@gmail.com>
2022-11-23 16:17:49 -0300

    added Card to teamList

--- app/home/teams/components/team-list/TeamList.tsx
+++ app/home/teams/components/team-list/TeamList.tsx
@@ -1,17 +1,57 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Teams } from "../../models";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import styledComponents from "styled-components";
+
+const CardContainer = styledComponents.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+`;
 
 interface Props {
   teams: Teams;
 }
 
 function TeamList({ teams }: Props) {
+  const handleClick = (website: string) => {
+    window.open(website, "_blank");
+  };
+
   return (
-    <div>
-      {teams.map((team) => {
-        return <p key={team.id}>{team.name}</p>;
-      })}
-    </div>
+    <>
+      <CardContainer>
+        {teams.map((team) => {
+          return (
+            <Card key={team.id}>
+              <CardHeader title={team.name} />
+              <Typography>
+                <a href={`mailto:${team.email}`}>{team.email}</a>
+              </Typography>
+              <CardMedia
+                component="img"
+                height="194"
+                image={team.crestUrl}
+                alt={team.name + " logo"}
+              />
+              <CardActions disableSpacing>
+                <Button size="small" onClick={() => handleClick(team.website)}>
+                  Go To Website
+                </Button>
+              </CardActions>
+            </Card>
+          );
+        })}
+      </CardContainer>
+    </>
   );
 }
 
